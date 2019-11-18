66d33af1ca3aa6467a16c3abc4b8819423863b25
Nikolay <acid_n@mail.ru>
2019-11-19 00:12:32 +0300

    accound and login

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     # Определенные ранее обработчики
     # path('login/', views.user_login, name='login'),
-    path('login/', auth_views.LoginView.as_view(), name='Login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
 ]
